a302869c511b3b5eb2663b50f4f2f6588989d321
Cynthia-ann Telfair <ctelfair20@gmail.com>
2023-05-03 00:50:34 -0500

    added more tests

--- src/components/globleBtn/globleBtn.test.tsx
+++ src/components/globleBtn/globleBtn.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen, cleanup } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import GlobleBtn from './GlobleBtn'
 
 afterEach(cleanup)
@@ -8,5 +9,24 @@ describe('GlobleBtn', () => {
     render(<GlobleBtn />)
     const globleIcon = screen.getByLabelText('globle-button')
     expect(globleIcon).toBeInTheDocument()
+  })
+
+  it('should have a container', () => {
+    render(<GlobleBtn />)
+    const globleContainer = screen.getByRole('button', { name: 'globle-button' })
+    const globleIcon = screen.getByLabelText('globle-button')
+
+    expect(globleContainer).toBeInTheDocument()
+    expect(globleIcon).toBeInTheDocument()
+    expect(globleContainer).toContainElement(globleIcon)
+  })
+
+  it('should have a light grey backdrop on hover', () => {
+    render(<GlobleBtn />)
+    const user = userEvent.setup()
+
+    const globleIcon = screen.getByLabelText('globle-button')
+    user.hover(globleIcon)
+    expect(globleIcon).toHaveStyle({ background: '#fff' })
   })
 })
