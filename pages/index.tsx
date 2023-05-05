4572c65799b963643c176b5deea733b8af5012c4
Cynthia-ann Telfair <ctelfair20@gmail.com>
2023-05-04 23:40:08 -0500

    updated snapshot and fixed weird test bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import ProfileBtn from '../src/components/profileBtn/ProfileBtn'
 import GlobeBtn from '../src/components/globeBtn/GlobeBtn'
-import AirbnbYourHomeBtn from 'src/components/airbnbYourHomeBtn/airbnbYourHomeBtn'
+import AirbnbYourHomeBtn from '../src/components/airbnbYourHomeBtn/airbnbYourHomeBtn'
 
 import styles from '@/pages/index.module.css'
 
