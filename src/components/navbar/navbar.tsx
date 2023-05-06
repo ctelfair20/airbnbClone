1001d0c08a47b0ff6f4da8ebe56406a1c9fef901
Cynthia-ann Telfair <ctelfair20@gmail.com>
2023-05-05 21:59:58 -0500

    created the navbar component and its styling

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-right">
+
+      </div>
+      <div className="nav-left">
+
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
