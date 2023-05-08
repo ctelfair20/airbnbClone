dbea6c49be590c53e3d45d0e6933bb189b685602
Cynthia-ann Telfair <ctelfair20@gmail.com>
2023-05-07 22:39:31 -0500

    added styles to the arrowBtn comp

--- src/components/arrowBtn/arrowBtn.tsx
+++ src/components/arrowBtn/arrowBtn.tsx
@@ -1,6 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons"
 
+import styles from '../../../styles/arrowBtn.module.css'
+
 const arrowBtn = () => {
   return (
     <div className="arrow-container">
