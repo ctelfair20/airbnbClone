2a330ba66e6523cb177f431e2a27c141ba4e9e65
Cynthia-ann Telfair <ctelfair20@gmail.com>
2023-05-08 22:38:16 -0500

    adds todo comment

--- src/components/optionsBar/OptionsBar.tsx
+++ src/components/optionsBar/OptionsBar.tsx
@@ -4,6 +4,7 @@ import ArrowBtn from '../arrowBtn/ArrowBtn'
 import styles from '../../../styles/optionsBar.module.css'
 
 const OptionsBar = () => {
+  //TODO: iterate through the array of images from the optionsBarIcons folder. Pass one 'option' to the option comp with each iteration
   return (
     <div className={styles['options-bar']}>
       <ArrowBtn />
