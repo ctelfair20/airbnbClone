efed7cefb88301c0a002d6719d6d1e7821adf006
Cynthia-ann Telfair <ctelfair20@gmail.com>
2023-05-11 14:03:24 -0500

    created style module for totalPriceToggle comp

--- src/components/totalPriceToggle/TotalPriceToggle.tsx
+++ src/components/totalPriceToggle/TotalPriceToggle.tsx
@@ -1,14 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faToggleOn, faToggleOff } from "@fortawesome/free-solid-svg-icons";
+import { faToggleOn, faToggleOff } from "@fortawesome/free-solid-svg-icons"
+
+import styles from '../../../styles/totalPriceToggle.module.css'
 
 const TotaLPriceToggle = () => {
   return (
-    <div className={style['price-toggle-container']}>
-      <span className={style['label']}>Display total price</span>
+    <div className={styles['price-toggle-container']}>
+      <span className={styles['label']}>Display total price</span>
       <div>
-        <span className={style['sub-label']}></span>
+        <span className={styles['sub-label']}></span>
       </div>
-      <FontAwesomeIcon icon={faToggleOn} />
+      <FontAwesomeIcon icon={faToggleOn} className={styles['toggle-icon']} />
 
     </div>
   );
