654036d9043df85baf6cb28d72056cb48f232954
Cynthia-ann Telfair <ctelfair20@gmail.com>
2023-05-12 11:37:00 -0500

    created css file for searchbar comp

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,6 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
+import styles from '../../../styles/searchbar.module.css'
+
 const SearchBar = () => {
   return (
     <div className={styles['searchbar-container']}>
@@ -9,7 +11,7 @@ const SearchBar = () => {
         <span>Any week</span>
         <span>Add guests</span>
       </div>
-      <FontAwesomeIcon icon={faMagnifyingGlass} className={ } />
+      <FontAwesomeIcon icon={faMagnifyingGlass} className={styles['search-icon']} />
     </div>
   );
 }
