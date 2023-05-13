ad017349ea133dd768c7c86e79e7ba2f2bb023f8
Cynthia-ann Telfair <ctelfair20@gmail.com>
2023-05-12 20:46:54 -0500

    finished styling for searchbar

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -7,7 +7,7 @@ const SearchBar = () => {
   return (
     <div className={styles['searchbar-container']}>
       <div className={styles['content']}>
-        <span className={styles['column']}>Anywhere</span>
+        <span className={`${styles['column']} ${styles['left']}`}>Anywhere</span>
         {/* This div allows the center text to have left and right borders the size of the text */}
         <div>
           <span className={`${styles['column']} ${styles['center']}`}>Any week</span>
