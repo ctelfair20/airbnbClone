e62952d1b587066ca0ad677d11e01737a2c90759
Cynthia-ann Telfair <ctelfair20@gmail.com>
2023-08-21 12:15:21 -0500

    changed button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,41 +1,73 @@
 import { useState } from "react";
 
 
+// const Button = () => {
+
+//   const [count, useCount] = useState(0)
+
+//   return (
+//     <div
+//       style={{
+//         'backgroundColor': "black",
+//         'width': '100px',
+//         'height': '100px',
+//         'borderRadius': '10px',
+//         'display': 'flex',
+//         'justifyContent': "center",
+//         'alignItems': "center",
+//         'flexDirection': 'column',
+
+//       }}>
+//       <div
+//         style={{
+//           'color': "white",
+//         }}
+//       >
+//         {count}
+//       </div>
+//       <div
+//         style={{
+//           'color': "white",
+//           'border': "solid 2px white",
+//           'padding': '2px',
+//           'borderRadius': '5px'
+//         }}
+//         onClick={() => useCount(count + 1)}
+//       >
+//         +</div>
+//     </div>
+//   );
+// }
+
+// export default Button;
+
 const Button = () => {
 
-  const [count, useCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div
+    <div className="container"
       style={{
-        'backgroundColor': "black",
+        'backgroundColor': "red",
         'width': '100px',
-        'height': '100px',
-        'borderRadius': '10px',
+        'height': "100px",
         'display': 'flex',
-        'justifyContent': "center",
-        'alignItems': "center",
-        'flexDirection': 'column',
-
-      }}>
-      <div
+        'justifyContent': 'center',
+        'alignItems': 'center',
+      }}
+    >
+      <div className="button" onClick={() => { setCount(count + 1) }}
         style={{
-          'color': "white",
+          'backgroundColor': "white",
+          'width': '30px',
+          'height': "30px",
+          'display': 'flex',
+          'justifyContent': 'center',
+          'alignItems': 'center',
         }}
       >
         {count}
       </div>
-      <div
-        style={{
-          'color': "white",
-          'border': "solid 2px white",
-          'padding': '2px',
-          'borderRadius': '5px'
-        }}
-        onClick={() => useCount(count + 1)}
-      >
-
-        +</div>
     </div>
   );
 }
